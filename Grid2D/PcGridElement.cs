2e2cb6605d82f8495cbd115bb3a974439b738f28
Brian Lee, Lord of Cinder and Primordial Ash <brianvanolee@gmail.com>
2016-10-18 21:51:03 -0400

    - Added Grid2D files - Added Utils - Added Parallax2D files - Added LICENSE file - Added README file

---
+++ Grid2D/PcGridElement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PcGridElement: MonoBehaviour 
+{
+	private PcGrid _grid;
+	public PcGrid Grid{
+		get { return _grid;}
+		set { _grid = value;}
+	}
+
+	[SerializeField]
+	private object _gridElementValue;
+	public object GridElementValue
+	{
+		get {return _gridElementValue;}
+		set { _gridElementValue = value;}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Utils/PcTuple.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct PcTuple <T,U>{
+
+	public T x;
+	public U y;
+
+	public PcTuple(T x, U y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+
+}
