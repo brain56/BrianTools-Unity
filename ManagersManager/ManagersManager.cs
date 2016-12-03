8db5a704c70dfc39795d2fb4eea2291d0d258fff
Brian Lee, Lord of Cinder and Primordial Ash <brianvanolee@gmail.com>
2016-12-03 17:03:16 -0500

    - Added AlphabetizeManagersList() - Changed line endings to Windows

--- ManagersManager/ManagersManager.cs
+++ ManagersManager/ManagersManager.cs
@@ -5,32 +5,44 @@ using System;
 
 namespace BrianTools.ManagersManager
 {
-    public class ManagersManager : MonoBehaviour
-    {
+	public class ManagersManager : MonoBehaviour
+	{
 
-        private static ManagersManager sInstance;
+		private static ManagersManager sInstance;
 
-        public static ManagersManager Instance
-        {
-            get
-            {
-                if (sInstance == null)
-                {
-                    GameObject prefab = Resources.Load("Prefabs/Managers/ManagersManager") as GameObject;
-                    GameObject instance = GameObject.Instantiate(prefab);
-                    sInstance = instance.GetComponent<ManagersManager>();
-                }
-                return sInstance;
-            }
-        }
+		public static ManagersManager Instance
+		{
+			get
+			{
+				if (sInstance == null)
+				{
+					GameObject prefab = Resources.Load("Prefabs/Managers/ManagersManager") as GameObject;
+					GameObject instance = GameObject.Instantiate(prefab);
+					sInstance = instance.GetComponent<ManagersManager>();
+				}
+				return sInstance;
+			}
+		}
 
-        [SerializeField]
-        private List<GameObject> _managers;
+		[SerializeField]
+		private List<GameObject> _managers;
 
-        public GameObject GetManagerPrefab<T>() where T : Component
-        {
-            GameObject prefab = _managers.Find(x => x.GetComponent<T>() != null);
-            return prefab;
-        }
-    }
+		public GameObject GetManagerPrefab<T>() where T : Component
+		{
+			GameObject prefab = _managers.Find(x => x.GetComponent<T>() != null);
+			return prefab;
+		}
+
+#if UNITY_EDITOR
+		[ContextMenu("AlphabetizeManagersList()")]
+		public void AlphabetizeManagersList()
+		{
+			_managers.Sort(delegate(GameObject a, GameObject b)
+			{
+				return a.name.CompareTo(b.name);
+			});
+		}
+#endif
+
+	}
 }
