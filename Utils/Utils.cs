cbe48c8fb75c45e56660e7ec33acafaf2f8edb5a
Brian Lee, Lord of Cinder and Primordial Ash <brianvanolee@gmail.com>
2017-01-09 13:30:59 -0500

    - Added DestroyChildren() utils function

--- Utils/Utils.cs
+++ Utils/Utils.cs
@@ -4,40 +4,40 @@ using System;
 
 namespace BrianTools
 {
-    public struct Tuple<T, U>
-    {
+	public struct Tuple<T, U>
+	{
 
-        public T x;
-        public U y;
+		public T x;
+		public U y;
 
-        public Tuple(T x, U y)
-        {
-            this.x = x;
-            this.y = y;
-        }
+		public Tuple(T x, U y)
+		{
+			this.x = x;
+			this.y = y;
+		}
 
-    }
+	}
 
-    public static class UtilExtensions
-    {
-        public static void SafeInvoke(this Action action)
-        {
-            if (action != null)
-            {
-                action();
-            }
-        }
+	public static class UtilExtensions
+	{
+		public static void SafeInvoke(this Action action)
+		{
+			if (action != null)
+			{
+				action();
+			}
+		}
 
-        public static void Normalize(this Transform trans)
-        {
-            trans.localPosition = Vector3.zero;
-            trans.localEulerAngles = Vector3.zero;
-            trans.localScale = Vector3.one;
-        }
+		public static void Normalize(this Transform trans)
+		{
+			trans.localPosition = Vector3.zero;
+			trans.localEulerAngles = Vector3.zero;
+			trans.localScale = Vector3.one;
+		}
 
 		public static void AddUnique<T>(this List<T> list, T item)
 		{
-			if(!list.Contains(item))
+			if (!list.Contains(item))
 			{
 				list.Add(item);
 			}
@@ -46,10 +46,20 @@ namespace BrianTools
 		public static void SetLayerRecursively(this GameObject gameObject, int layer)
 		{
 			gameObject.layer = layer;
-			foreach(Transform t in gameObject.transform)
+			foreach (Transform t in gameObject.transform)
 			{
 				t.gameObject.SetLayerRecursively(layer);
 			}
 		}
-    }
+		
+		public static void DestroyChildren(this Transform transform)
+		{
+			while(transform.childCount > 0)
+			{
+				Transform child = transform.GetChild(0);
+				child.SetParent(null);
+				GameObject.Destroy(child.gameObject);
+			}
+		}
+	}
 }
